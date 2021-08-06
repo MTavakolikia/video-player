8f04ce34d1861884d69ae8712031bea198cad68e
mohammad tavakoli <mohammadtavakolikia66@gmail.com>
2021-08-06 15:35:25 +0430

    Volume is now functional.

--- script.js
+++ script.js
@@ -31,14 +31,66 @@ function showPlayIcon(){
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time){
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds>9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 // Update Progress Bar as video plays
 function updateProgress() {
-    progressBar.style.width=`${(video.currentTime/video.duration)*100}%`;
+    progressBar.style.width = `${(video.currentTime/video.duration)*100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
   }
 
+  // Click to seek within the video
+function setProgress(e){
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime= newTime * video.duration;
+}
+
 // Volume Controls --------------------------- //
+let lastVolume=1;
+// Change Volume
+function changeVolume(e){
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    volumeBar.style.width=`${(volume * 100)}%`;
+    video.volume=volume;
+    if(volume < 0.1 ){
+        volume = 0;
+    }else if(volume > 0.9 ){
+        volume = 1;
+    }
+volumeIcon.className='';
+    if(volume > 0.7 ){
+        volumeIcon.classList.add('fas','fa-volume-up');
+    }else if(volume < 0.7 && volume > 0){
+        volumeIcon.classList.add('fas','fa-volume-down');
+    }else if(volume === 0){
+        volumeIcon.classList.add('fas','fa-volume-off');
+    }
+    lastVolume=volume;
+}
 
-
+// Mute/Unmute
+function toggleMute(){
+    volumeIcon.className='';
+    if (video.volume) {
+        lastVolume=video.volume;
+        video.volume=0;
+        volumeBar.style.width=0;
+        volumeIcon.classList.add('fas','fa-volume-mute');
+        volumeIcon.setAttribute('title','Unmute');
+    } else {
+        video.volume=lastVolume;
+        volumeBar.style.width=`${lastVolume * 100}%`;
+        volumeIcon.classList.add('fas','fa-volume-up');
+        volumeIcon.setAttribute('title','Mute');
+    }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -53,3 +105,6 @@ video.addEventListener('click',togglePlay);
 video.addEventListener('ended',showPlayIcon);
 video.addEventListener('timeupdate',updateProgress);
 video.addEventListener('canplay',updateProgress);
+progressRange.addEventListener('click',setProgress);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click',toggleMute);
